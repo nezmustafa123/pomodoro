73016d6e3dc24f1c120e097fba0f613831f84840
nezmustafa123 <nezmustafa123@gmail.com>
2020-05-28 19:43:46 +0100

    changed variables to global and added functions to make it more DRY

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
 var minutes = 25;
-var seconds = "00";
-
+var seconds = 0;
+var myInterval = 0;
 //initialise two sounds we have before
 var click = new Audio('click.mp3');
 var bell = new Audio('bell.mp3');
 
+var pause = document.querySelector('.btn_pause');
+
+function formatSeconds(sec) {
+    if(sec === 0 ) {
+        return "00"
+        
+    }
+    return sec
+}
+
 function template() {
     document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("seconds").innerHTML = formatSeconds(seconds);
 
 }
+
+//gloval variables
+// minutes = 24;
+// seconds = 59;
 
 function start() {
     click.play();
     
     //change number in timer
     
-    minutes = 24;
-    seconds = 59;
+  
     
     document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("seconds").innerHTML = formatSeconds(seconds);
     
     //minutes timer function fires every 60 seconds
     //seconds timer function fires every second
-    var minutes_interval = setInterval(minutesTimer, 60000);
+//    var minutes_interval = setInterval(minutesTimer, 60000);
     var seconds_interval = setInterval(secondsTimer, 1000);
 
 //has to be declared inside the start function
 
 function minutesTimer() {
     //will fire every 6000 milliseconds
-    //and decrease the mminnunte number 
+    //and decrease the minunte number 
     minutes = minutes - 1;
     document.getElementById("minutes").innerHTML = minutes;
 }
 
 function secondsTimer() {
     //will fire every 6000 milliseconds
-    //and decrease the mminnunte number 
+    //and decrease the minute number 
     seconds = seconds - 1;
-    document.getElementById("seconds").innerHTML = seconds;
-    if(seconds <= 0) {
+  
+    if(seconds === -1) {
+        
+       minutesTimer();
         //whenever timer goes to zero at that point the timer has to stop 
         //so reset interval
-        if(minutes <= 0 ) {
-            clearInterval(minutes_interval);
+        if(minutes === 0 ) {
+//            clearInterval(minutes_interval);
             clearInterval(seconds_interval);
             
             document.getElementById("done").innerHTML = "Session completed!! Take a Break";
@@ -54,9 +69,19 @@ function secondsTimer() {
 
              bell.play();
          }
-        seconds = 60;
+        seconds = 59;
      }
+    document.getElementById("seconds").innerHTML = formatSeconds(seconds);
    }
-}
+
+    
+    pause.addEventListener('click', function(e) {
+       e.preventDefault();
+//        clearInterval(minutes_interval);
+        clearInterval(seconds_interval);
+    }
+
+)};
+
 
 
